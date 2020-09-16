8c78b81c4cdc17a28deb14301b8ada4c1f675f20
Trey Manuszak <manuszaktrey@gmail.com>
2020-09-16 12:24:33 -0700

    HW2_2 complete

---
+++ HW2/HW2_2.c
@@ -0,0 +1,36 @@
+/*
+ * Assignment: Homework #2
+ * Name: Trey Manuszak
+ * ASU email: tmanusza@asu.edu
+ * Course: CSE240 TTh 1030-1145
+ * File Description:  C program to display multiplication table for a given number. 
+ * Accept the number from the user. (Multiplication from InputNumber*1 to InputNumber *10)
+ * */
+
+#include <stdio.h>
+
+int main()
+{
+    int inputNumber;
+    int table[10];
+
+    // Get inputNumber
+    printf("Enter a number: ");
+    scanf("%d", &inputNumber);
+
+    // Generate multiplication table
+    for (int i = 0; i < 10; i++)
+    {
+        table[i] = inputNumber * (i + 1);
+    }
+
+    // Print table
+    printf("*\t1\t2\t3\t4\t5\t6\t7\t8\t9\t10\n");
+    printf("%d", inputNumber);
+    for (int i = 0; i < 10; i++)
+    {
+        printf("\t%d", table[i]);
+    }
+
+    return 0;
+}

334000ad0e3e89fd527ade96542e4fb062a080b2
Trey Manuszak <manuszaktrey@gmail.com>
2020-11-30 11:39:36 -0700

    Finished HW6

--- HW6/homework.pl
+++ HW6/homework.pl
@@ -3,7 +3,8 @@
 % ASU email: tmanusza@asu.edu
 % Course: CSE240 TTh 1030-1145
 % File Description: Main file to HW6 showcases knowledge of Prolog
-%			by using facts, rules, and functions.
+%			by using facts, rules, and functions. This program
+%			was compiled using the GNU Prolog Compiler.
 
 % Question 2 "Weather is cold. If weather is cold, everyone likes it."
 cold(weather).
@@ -35,16 +36,45 @@ vegetable(carrot).
 vegetable(egg_plant).
 likes(jose, X) :- vegetable(X).
 loves(Who, egg_plant) :- vegetarian(Who).
-% vegetable(X).
+%--------------
+% vegetable(X). 
+% Answer:
+% X = carrot
+% X = egg_plant
+% yes
+%----------------
 % vegetable(tomato).
+% Answer:
+% no
+%----------------
 % likes(jose, What).
+% Answer (if question 5 program is in same file as question 2 since likes calls the function
+%		in question 1 if they are in the same file):
+% What = weather
+% yes
+% Answer (if question 5 is in seperate file from question 2):
+% What = carrot
+% What = egg_plant
+% yes
+%----------------
 % likes(Who, egg_plant).
+% Answer (if question 5 program is in same file as question 2 since likes calls the function
+%		in question 1 if they are in the same file):
+% no
+% Answer (if question 5 is in seperate file from question 2):
+% Who = jose
+% yes
+%-----------------
 % loves(Who, egg_plant).
+% Answer:
+% Who = jose
+% Who = james
+% yes
 
 % Question 6
-% fib(1,1).
-% fib(2,1).
-% fib(N,R) :- N >= 3, N1 is N-1, N2 is N-2, fib(N1,R1), fib(N2,R2), R is R1+R2.
+ fib(1,1).
+ fib(2,1).
+ fib(N,R) :- N >= 3, N1 is N-1, N2 is N-2, fib(N1,R1), fib(N2,R2), R is R1+R2.
 %
 % a: Let F_n denote the nth fibonacci number. Then fib(1,1). represents F_1 = 1
 %	and fib(2,1). represents F_2 = 1. Then, the rule fib(N,R) is stated such
@@ -56,7 +86,7 @@ loves(Who, egg_plant) :- vegetarian(Who).
 %	which then returns R = 3 to the console.
 %
 % c: fib(9, R). returns R = 34
-%	fib(9,9). returns False
+%	fib(9,9). returns no
 
 % Question 7
 % a: reads_history(john).
